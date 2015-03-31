b23b58e9759daf3a38959aff1f6402e80c1da23e
flmerto <francis_lynch.merto@robertsoncollege.net>
2015-03-31 13:35:47 -0500

    Added the project

---
+++ MVC_Group_Project/MVC_Group_Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_Group_Project.Startup))]
+namespace MVC_Group_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
