9a932eb7d730b09b6dbb1f9b2203652d50881464
flmerto <francis_lynch.merto@robertsoncollege.net>
2015-04-15 16:05:14 -0500

    fixed some labels and added Transcations model

---
+++ MVC_Group_Project/MVC_Group_Project/Models/Transactions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Group_Project.Models
+{
+    public class Transactions
+    {
+        public int TransactionsID { get; set; }
+        public DateTime PurchaseDateTime { get; set; }
+        public Double Amount { get; set; }
+        public int BiddingItemID { get; set; }
+        [ForeignKey("BiddingItemID")]
+        public BiddingItem BidItem { get; set; }
+        public int CreditCardID { get; set; }
+        [ForeignKey("CreditCardID")]
+        public CreditCard Credcard { get; set; }
+    }
+}
