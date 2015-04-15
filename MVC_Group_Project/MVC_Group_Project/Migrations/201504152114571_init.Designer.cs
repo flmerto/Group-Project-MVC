eb0972f1f10371ae02aa52350f121ed25274bd0b
Caleb27 <catheryn.ablaza@robertsoncollege.com>
2015-04-15 16:30:48 -0500

    edit identity config

--- MVC_Group_Project/MVC_Group_Project/Migrations/201504152114571_init.Designer.cs
+++ MVC_Group_Project/MVC_Group_Project/Migrations/201504152114571_init.Designer.cs
@@ -13,7 +13,7 @@ namespace MVC_Group_Project.Migrations
         
         string IMigrationMetadata.Id
         {
-            get { return "201504152009278_init"; }
+            get { return "201504152114571_init"; }
         }
         
         string IMigrationMetadata.Source
